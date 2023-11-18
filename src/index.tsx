4a36d1718ec709110cca39d72db1d65481fe4c9a
wanku-x <wanku@bk.ru>
2023-11-18 23:12:55 +0400

    ESLint configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App'
 
-import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+import addOnUISdk from 'https://new.express.adobe.com/static/add-on-sdk/sdk.js'
 
 addOnUISdk.ready.then(() => {
-	const root = createRoot(document.getElementById("root"));
-	root.render(<App addOnUISdk={addOnUISdk} />);
-});
+	const rootElement = document.getElementById('root')
+
+	if (rootElement === null) throw new Error('Root element not found')
+
+	const root = createRoot(rootElement)
+	root.render(<App addOnUISdk={addOnUISdk} />)
+}).catch(err => {
+	console.error(err)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,56 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-const isEnvProduction = process.env.NODE_ENV === "production";
+const isEnvProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	mode: isEnvProduction ? "production" : "development",
-	devtool: isEnvProduction ? "source-map" : "eval-source-map",
-	entry: "./src/index.tsx",
+	mode: isEnvProduction ? 'production' : 'development',
+	devtool: isEnvProduction ? 'source-map' : 'eval-source-map',
+	entry: './src/index.tsx',
 	experiments: {
-		outputModule: true
+		outputModule: true,
 	},
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, 'dist'),
 		module: true,
-		filename: "index.js"
+		filename: 'index.js',
 	},
-	externalsType: "module",
+	externalsType: 'module',
 	externalsPresets: { web: true },
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: "src/index.html",
-			scriptLoading: "module"
+			template: 'src/index.html',
+			scriptLoading: 'module',
 		}),
 		new CopyWebpackPlugin({
-			patterns: [{ from: "src/*.json", to: "[name][ext]" }]
-		})
+			patterns: [{ from: 'src/*.json', to: '[name][ext]' }],
+		}),
+		new ESLintPlugin({
+			extensions: ['js', 'jsx', 'ts', 'tsx'],
+			emitWarning: isEnvProduction,
+			emitError: isEnvProduction,
+		}),
 	],
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/
+				use: 'ts-loader',
+				exclude: /node_modules/,
 			},
 			{
 				test: /(\.pcss)$/,
 				use: [
-					"style-loader",
-					"css-loader",
-					"postcss-loader",
-				]
-			}
-		]
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+				],
+			},
+		],
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js", "css"]
-	}
-};
+		extensions: ['.tsx', '.ts', '.js', '.pcss'],
+	},
+}
